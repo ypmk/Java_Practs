bf0c4a40e2c8a0ca8d9d2843283ac5fd00b98870
Kopylova_Ksenia <55356699+ypmk@users.noreply.github.com>
2022-11-22 00:55:22 +0300

    Create 6.java

---
+++ P_1/6.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("1");
+        for (int i = 1; i < 10; i++) {
+            System.out.println("1/"+i);
+        }
+    }
+}
