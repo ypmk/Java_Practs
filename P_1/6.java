feab9394855261f086fe6a747ea7310400df8659
Kopylova_Ksenia <55356699+ypmk@users.noreply.github.com>
2022-11-22 00:56:40 +0300

    Update 6.java

--- P_1/6.java
+++ P_1/6.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("1");
+        System.out.print("1  ");
         for (int i = 1; i < 10; i++) {
-            System.out.println("1/"+i);
+            System.out.print("1/"+i+"  ");
         }
     }
 }
